52c6e5205b96e3eccdb577712e6bf994c7092b88
Gentlesammy <samodunlade84@gmail.com>
2021-11-26 12:09:47 +0100

    basic connect and disconnect

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import {Web3ReactProvider} from "@web3-react/core"
+import web3 from "web3"
 
+function getLirary(provider){
+  return new web3(provider)
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <Web3ReactProvider  getLibrary={getLirary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
